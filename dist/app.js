d48cffcea54edbd1347b1894ae9f69752b5fc1c8
Gyan Ankit <g.gyanankit@gmail.com>
2022-02-05 19:12:44 +0530

    project re-structured

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fileupload = require('express-fileupload')
 const cors = require('cors');
+const path = require('path');
 
 const db = require('./config/database');
 const middleware = require('./helper/middleware');
@@ -14,6 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(fileupload({ createParentPath: true }));
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, '/views')))
 
 app.use('/web', middleware, webRouter);
 
